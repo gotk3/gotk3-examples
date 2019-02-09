f2a3ca11ddc2b7b7e3e32d8461fee2e0b14fbb5c
Matt <drakbar96@gmail.com>
2019-02-09 16:40:10 -0600

    Update glade.go
    Refactored to use GtkApplication as per https://wiki.gnome.org/HowDoI/GtkApplication.

--- gtk-examples/glade/glade.go
+++ gtk-examples/glade/glade.go
@@ -1,59 +1,81 @@
 package main
 
 import (
-    "log"
-    "errors"
+	"errors"
+	"log"
+	"os"
 
-    "github.com/gotk3/gotk3/gtk"
-    "github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
 )
 
+const appId = "com.github.gotk3.gotk3-examples.glade"
+
 func main() {
-    // Initialize GTK without parsing any command line arguments.
-    gtk.Init(nil)
+	// Create a new application.
+	application, err := gtk.ApplicationNew(appId, glib.APPLICATION_FLAGS_NONE)
+	errorCheck(err)
 
-    // Get the GtkBuilder UI definition in the glade file.
-    builder, err := gtk.BuilderNewFromFile("ui/example.glade")
-    errorCheck(err)
+	// Connect function to application startup event, this is not required.
+	application.Connect("startup", func() {
+		log.Println("application startup")	
+	})
 
-    // Map the handlers to callback functions, and connect the signals 
-    // to the Builder. 
-    signals := map[string]interface{} {"destroy": on_window_destroy}
-    builder.ConnectSignals(signals)
+	// Connect function to application activate event
+	application.Connect("activate", func() {
+		log.Println("application activate")
 
-    // Get the object with the id of "window". 
-    obj, err := builder.GetObject("window")
-    errorCheck(err)
+		// Get the GtkBuilder UI definition in the glade file.
+		builder, err := gtk.BuilderNewFromFile("ui/example.glade")
+		errorCheck(err)
 
-    // Verify that it is a pointer to a Window Widget.
-    win, err := isWindow(obj)
-    errorCheck(err)
-    
-    // Show the Widget.
-    win.Show()
+		// Map the handlers to callback functions, and connect the signals
+		// to the Builder.
+		signals := map[string]interface{}{
+			"on_main_window_destroy": onMainWindowDestroy,
+		}
+		builder.ConnectSignals(signals)
 
-    // Begin executing the GTK main loop.  This blocks until
-    // gtk.MainQuit() is run.
-    gtk.Main()
+		// Get the object with the id of "main_window".
+		obj, err := builder.GetObject("main_window")
+		errorCheck(err)
+
+		// Verify that the object is a pointer to a gtk.ApplicationWindow.
+		win, err := isWindow(obj)
+		errorCheck(err)
+
+		// Show the Window and all of its components.
+		win.Show()
+		application.AddWindow(win)
+	})
+
+	// Connect function to application shutdown event, this is not required.
+	application.Connect("shutdown", func() {
+		log.Println("application shutdown")	
+	})
+
+	// Launch the application
+	os.Exit(application.Run(os.Args))
 }
 
 func isWindow(obj glib.IObject) (*gtk.Window, error) {
-    // Make type assertion (as per gtk.go).
-    if win, ok := obj.(*gtk.Window); ok {
-      return win, nil
-    } 
-    return nil, errors.New("not a *gtk.Window")
+	// Make type assertion (as per gtk.go).
+	if win, ok := obj.(*gtk.Window); ok {
+		return win, nil
+	}
+	return nil, errors.New("not a *gtk.Window")
 }
 
 func errorCheck(e error) {
-    if e != nil {
-      // panic for any errors.
-      log.Panic(e)
-    }
+	if e != nil {
+		// panic for any errors.
+		log.Panic(e)
+	}
 }
 
-func on_window_destroy() {
-    log.Println("on_window_destroy")
-    // Terminate the GTK main loop.
-    gtk.MainQuit()
+// onMainWindowDestory is the callback that is linked to the 
+// on_main_window_destroy handler. It is not required to map this, 
+// and is here to simply demo how to hook-up custom callbacks.
+func onMainWindowDestroy() {
+	log.Println("onMainWindowDestroy")
 }
